6bfe1c5631bd1a836c1f90994e6d805f851902e8
hung1201 <hung1201998@gmail.com>
2021-04-06 15:27:29 +0700

    fixed config

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const path = require('path')
 // ======================================================
 //Setting config file
 if(process.env.NODE_ENV !== 'PRODUCTION') 
-    require('dotenv').dotenv.config({path:'backend/config/config.env'})
+    require('dotenv').config({path:'backend/config/config.env'})
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ process.on('uncaughtException',err => {
 
 //Setting config file
 if(process.env.NODE_ENV !== 'PRODUCTION') 
-    require('dotenv').dotenv.config({path:'backend/config/config.env'})
+    require('dotenv').config({path:'backend/config/config.env'})
 
 
 //Connect to db
